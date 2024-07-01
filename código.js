52ece333ce3a38df45a8c84cd42f60888c3d552c
Raul <soaresraul22@gmail.com>
2024-07-01 09:21:46 -0300

    feat: adicionado todo o código

---
+++ código.js
@@ -0,0 +1,21 @@
+console.log("Digite um número para obter sua maior combinação: ");
+let numero = "";
+let maior = [];
+process.stdin.on("data", function (data) {
+  numero = data.toString().trim();
+  numero = numero.split("");
+  for (let i = 0; i < numero.length; i++) {
+    for (let j = 0; j < numero.length; j++) {
+      let aux = numero[i];
+      numero[i] = numero[j];
+      numero[j] = aux;
+      if (!maior.includes(numero.join(""))) maior.push(numero.join(""));
+    }
+  }
+
+  var array = [];
+  array.push(maior[maior.length - 1]);
+
+  console.log("O maior número será: " + array);
+  process.exit();
+});
